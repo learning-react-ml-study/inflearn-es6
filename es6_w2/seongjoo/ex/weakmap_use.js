75b9ccad567e3490d3675b05f8d385fb3ef5d0e2
jinseongjoo <qpyou1234@naver.com>
2018-07-02 00:42:29 +0900

    #seongjoo comment ES6 week2 Map & WeakMap 정리 끝

---
+++ es6_w2/seongjoo/ex/weakmap_use.js
@@ -0,0 +1,32 @@
+// const wm = new WeakMap();
+const obj = {};
+
+function Area(height,width) {
+    // wm.set(this, {height,width});
+    obj.height = height;
+    obj.width = width;
+}
+
+Area.prototype.getArea = function() {
+    /*const {height, width} =  wm.get(this);
+    return height*width;*/
+    // return this.height*this.width;
+    return obj.height * obj.width;
+
+}
+
+let myarea = new Area(10,20);
+
+//console.log(myarea.getArea());
+/*
+console.log(wm.has(myarea));
+
+myarea = null;
+
+console.log(wm.has(myarea));*/
+
+console.log(obj);
+
+myarea = null;
+
+console.log(obj)
