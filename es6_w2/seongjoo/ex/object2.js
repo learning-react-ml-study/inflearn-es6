02986fbe2412d92da2cd9a67c3cff24e6a5e6f2b
jinseongjoo <qpyou1234@naver.com>
2018-07-01 19:40:00 +0900

    #seongjoo comment ES6 week2 간단히 객체생성하기

---
+++ es6_w2/seongjoo/ex/object2.js
@@ -0,0 +1,20 @@
+function getObj() {
+    const name = "crong";
+
+    const getName = function() {
+        return name;
+    }
+
+    const setName = function(newname) {
+        name = newname;
+    }
+
+    const printName = function() {
+        console.log(name);
+    }
+
+    return {getName,setName,name}
+}
+
+var obj = getObj();
+console.log(obj);
