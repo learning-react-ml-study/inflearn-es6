1ac4520ea4297850131912fb4934aedfc8cea62e
jinseongjoo <qpyou1234@naver.com>
2018-07-15 17:32:51 +0900

    #seongjoo comment ES6 week4 미니프로젝트 babel preset 설정 완료

---
+++ es6_w4/seongjoo/mini_project/src/index.js
@@ -0,0 +1,6 @@
+console.log('start project by webpack');
+const myfun = () => {
+    let name = 'seongjoo'
+    console.log(`my name is ${name}`)
+};
+myfun();
---
+++ es6_w4/seongjoo/readme.md
@@ -0,0 +1,7 @@
+# 미니 프로젝트
+
+## 왜 바벨이 필요한가?
+
+http://kangax.github.io/compat-table/es6/
+
+IE때문에 필요함
